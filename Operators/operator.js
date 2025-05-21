3e31ada3c6f1ced23a42aa4603af468c37185373
hiclassic <hares.ahmed.319@gmail.com>
2025-05-22 00:36:50 +0600

    Operator Update

--- Operators/operator.js
+++ Operators/operator.js
@@ -23,4 +23,54 @@ console.log(e);
 console.log(f);
 console.log(g);
 
+m = (15-8) * 10 / 7 + 4**4;
+console.log(m);
+
+var lin1 ="Hello";
+var lin2 = "World";
+console.log( lin1 + lin2);
+var _lin1 ="Dhaka ";
+var _lin2 = "Bangladesh";
+console.log(_lin1 + _lin2);
+
+var _1 = 36 + 4;
+var _k = "Hello"
+console.log(_1 + _k);
+var _a = "Taka" + 44 + 55
+console.log(_a);
+var _j = 20+30+40+ "Taka"
+console.log(_j);
+var mm = "99" + "55"
+console.log(mm);
+var tt = "66" - "33";
+console.log(tt);
+var ty = "96" - "Tom";
+console.log(ty);
+let x = "10";
+console.log( x += 5);
+
+let y = 10;
+console.log( y += 5);
+console.log( y -= 5);
+console.log( y *= 5);
+console.log( y /= 5);
+console.log( y %= 3);
+console.log( y **= 5);
+
+let text1 = "What a very ";
+console.log(text1 += "nice day");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

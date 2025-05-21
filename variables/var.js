f6da6eb366ab65e3650adc2587e1edad76823c79
hiclassic <hares.ahmed.319@gmail.com>
2025-05-21 19:19:41 +0600

    Js_Variables Part

---
+++ variables/var.js
@@ -0,0 +1,46 @@
+var a;
+let b;
+a =45;
+b ="hiclassic";
+console.log(a);
+a =49.36;
+console.log(a);
+console.log(a);
+a ="goal"
+console.log(a);
+console.log(b);
+var  c = a;
+console.log(c);
+var d = a + b;
+console.log(d);
+
+var $a = 30;
+console.log($a);
+
+var _abc, abc123;
+_abc = 96;
+abc123 =85;
+console.log(_abc);
+console.log(abc123);
+console.log(_abc + abc123);
+
+var Myname, myName;
+Myname = "abcd";
+myName = "xyz";
+console.log(myName);
+console.log(Myname);
+
+var v =99;
+console.log(v);
+var v;
+console.log(v);
+
+const u =777;
+console.log(u);
+
+
+
+
+
+
+

fc1a3d4f00bd24e91485d644e5ef2f04e7fc49e1
hiclassic <hares.ahmed.319@gmail.com>
2025-05-21 22:26:59 +0600

    var update

---
+++ Operators/operator.js
@@ -0,0 +1,26 @@
+var a = 10;
+var b = 15;
+var c = a + b;
+console.log(c);
+console.log( a * b);
+console.log( a ** b);
+
+a++;
+++a;
+b--;
+--b;
+console.log(a++);
+console.log(++a);
+console.log(b--);
+console.log(--b);
+
+d = a++;
+e = ++a;
+f = b--;
+g = --b;
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+
+

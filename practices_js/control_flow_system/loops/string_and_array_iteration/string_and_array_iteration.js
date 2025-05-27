fb07f188c921a7f2617cbefe3e514a6446148006
hiclassic <hares.ahmed.319@gmail.com>
2025-05-27 17:51:53 +0600

    Auto sync - 2025-05-27_17-51-52

---
+++ practices_js/control_flow_system/loops/string_and_array_iteration/string_and_array_iteration.js
@@ -0,0 +1,29 @@
+// String
+
+let wor = "Bangladesh";
+for ( var i = 0; i <= 10; i++){
+    console.log(`Index : ${i}`);
+    console.log(wor[i]);
+};
+
+
+let word ="Pneumonoultramicroscopicsilicovolcanoconiosis";
+let len = word.length
+for ( var i = 0; i <=len; i++){
+    console.log(`Index : ${i}`);
+    console.log(word[i]);
+};
+
+let wrd ="Pneumonoultramicroscopicsilicovolcanoconiosis";
+let leng = word.length
+for ( var i = 0; i < leng; i++){
+    console.log(`Index : ${i}`);
+    console.log(word[i]);
+};
+
+let food = [ "apple"," mango", "banana", "lichy"];
+let ln = food.length;
+for ( var i = 0; i < ln; i++){
+    console.log(`Index: ${i}`);
+    console.log(food[i]);
+}

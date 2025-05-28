c9413d4e419149b47190ed6fa9cb9115e8257028
hiclassic <hares.ahmed.319@gmail.com>
2025-05-28 22:50:06 +0600

    Auto sync - 2025-05-28_22-50-05

--- practices_js/control_flow_system/loops/for_in_for_of/for_in_for_of.js
+++ practices_js/control_flow_system/loops/for_in_for_of/for_in_for_of.js
@@ -1,6 +1,8 @@
 
 let name = "I am learning javascript!";
 let food = ["cake", "chocolate","Ice cream" ];
+
+// object doesn't have index;
 let person = {
     Name:"shakib al hasan",
     profession: "student",
@@ -11,8 +13,40 @@ let person = {
 };
 
 // for in string/array/object;
+//  we will get index;
+
 
 for( var x in name){
-    console.log(name);
+    // console.log(name);
     console.log(`index ${x} and item ${name[x]}`);
+};
+
+//  for of string/array;
+//  we will get items/value;
+for ( var x of name){
+    console.log(x);
+};
+
+
+for( var y in food){
+ 
+    console.log(`index ${y} and item ${food[y]}`);
+};
+
+for( var y of food){
+   
+    console.log(y);
+};
+
+
+for ( var obj in person ){
+    console.log(`Property: ${obj} 
+        value: ${person}`);
 }
+
+// we don't iterate object with for_of ;
+
+// for ( var i of person){
+//     console.log(x);
+// }
+
